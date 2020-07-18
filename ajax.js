c88fb7397e807d334f6fa2441815c1d795e34713
ohad <ohadas88@gmail.com>
2020-07-18 12:00:51 +0300

    successfully getting the object and throwing error

--- ajax.js
+++ ajax.js
@@ -1,7 +1,7 @@
 function getApi() {
   return $.ajax({
     url:
-      "ssss https://gorest.co.in/public-api/users?_format=json&access-token=TCCLGqkQi-iO6ptX2j-ZfvNvLqvGE7p7R9xC",
+      "https://gorest.co.in/public-api/users?_format=json&access-token=TCCLGqkQi-iO6ptX2j-ZfvNvLqvGE7p7R9xC",
     method: "GET",
     success: function (results) {
       console.log(results);
