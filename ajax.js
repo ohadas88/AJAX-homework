c2794b1b213b99e07a069a8ea769d661ad3ccc74
ohad <ohadas88@gmail.com>
2020-07-18 13:09:52 +0300

    finished

--- ajax.js
+++ ajax.js
@@ -1,13 +1,13 @@
-function getApi() {
-  return $.ajax({
-    url:
-      "https://gorest.co.in/public-api/users?_format=json&access-token=TCCLGqkQi-iO6ptX2j-ZfvNvLqvGE7p7R9xC",
-    method: "GET",
-    success: function (results) {
-      return drawData(results);
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      alert("ERROR!");
-    },
-  });
-}
+// function getApi() {
+//   return $.ajax({
+//     url:
+//       "https://gorest.co.in/public-api/users?_format=json&access-token=TCCLGqkQi-iO6ptX2j-ZfvNvLqvGE7p7R9xC",
+//     method: "GET",
+//     success: function (results) {
+//       return drawData(results);
+//     },
+//     error: function (jqXHR, textStatus, errorThrown) {
+//       alert("ERROR!");
+//     },
+//   });
+// }
--- app.js
+++ app.js
@@ -15,7 +15,21 @@ async function drawData() {
 }
 
 function _drawData(objects) {
-  return console.log("WE ARE IN");
+  const result = objects.result;
+  const tableBody = $("#TableBody");
+  result.forEach((user) => {
+    const tr = $("<tr></tr>");
+    const tdName = $(`<td><b>${user.first_name}</b></td>`);
+    const tdLastname = $(`<td>${user.last_name}</td>`);
+    const tdGender = $(`<td>${user.gender}</td>`);
+    const tdId = $(`<td>${user.id}</td>`);
+    const tdEmail = $(`<td>${user.email}</td>`);
+    const tdAddress = $(`<td>${user.address}</td>`);
+
+    tr.append(tdName, tdLastname, tdGender, tdId, tdEmail, tdAddress);
+    tableBody.append(tr);
+  });
+  return tableBody;
 }
 
 drawData();
